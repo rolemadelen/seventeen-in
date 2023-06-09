7dccce37fbb6c8c5ce8c989343c2a22151c1cb8c
rolemadelen <rolemadelen@pm.me>
2023-06-08 23:41:31 -0400

    seventeen updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
 pub struct Seventeen;
 
 impl Seventeen {
-    pub fn number(&self) -> u32 {
+    pub fn in_number(&self) -> u32 {
         17
     }
 
-    pub fn string(&self) -> String {
+    pub fn in_string(&self) -> String {
         String::from("seventeen")
     }
 
-    pub fn all_languages(&self) -> Vec<(String, String)> {
+    pub fn in_all_languages(&self) -> Vec<(String, String)> {
         vec![
             (String::from("en"), String::from("seventeen")),
             (String::from("ko"), String::from("열일곱")),
@@ -19,18 +19,18 @@ impl Seventeen {
         ]
     }
 
-    pub fn english(&self) -> Vec<String> {
+    pub fn in_english(&self) -> Vec<String> {
         vec![String::from("seventeen")]
     }
 
-    pub fn korean(&self) -> Vec<String> {
+    pub fn in_korean(&self) -> Vec<String> {
         vec![
             String::from("열일곱"),
             String::from("십칠")
         ]
     }
 
-    pub fn japanese(&self) -> Vec<String> {
+    pub fn in_japanese(&self) -> Vec<String> {
         vec![
             String::from("十七"), 
             String::from("じゅナナ"),
@@ -38,43 +38,43 @@ impl Seventeen {
         ]
     }
 
-    pub fn japanese_kanji(&self) -> Vec<String> {
+    pub fn in_japanese_kanji(&self) -> Vec<String> {
         vec![String::from("十七")]
     }
 
-    pub fn japanese_hiragana(&self) -> Vec<String> {
+    pub fn in_japanese_hiragana(&self) -> Vec<String> {
         vec![String::from("じゅナナ")]
     }
 
-    pub fn japanese_katakana(&self) -> Vec<String> {
+    pub fn in_japanese_katakana(&self) -> Vec<String> {
         vec![String::from("ジュナナ")]
     }
 
-    pub fn roman_numeral(&self) -> Vec<String> {
+    pub fn in_roman_numeral(&self) -> Vec<String> {
         vec![String::from("XVII")]
     }
 
-    pub fn french(&self) -> Vec<String> {
+    pub fn in_french(&self) -> Vec<String> {
         vec![String::from("dix-sept")]
     }
 
-    pub fn decimal(&self) -> Vec<String> {
+    pub fn in_decimal(&self) -> Vec<String> {
         vec![String::from("17")]
     }
 
-    pub fn binary(&self) -> Vec<String> {
+    pub fn in_binary(&self) -> Vec<String> {
         vec![format!("{:b}", 17)]
     }
 
-    pub fn hex(&self) -> Vec<String> {
+    pub fn in_hex(&self) -> Vec<String> {
         vec![format!("{:x}", 17)]
     }
 
-    pub fn oct(&self) -> Vec<String> {
+    pub fn in_oct(&self) -> Vec<String> {
         vec![format!("{:o}", 17)]
     }
 
-    pub fn is_seventeen_number(&self, x: i32) -> Result<bool, String> {
+    pub fn is_number_seventeen(&self, x: i32) -> Result<bool, String> {
         if x == 17 {
             Ok(true)
         } else {
@@ -82,8 +82,8 @@ impl Seventeen {
         }
     }
 
-    pub fn is_seventeen_text(&self, s: &String) -> Result<bool, String> {
-        let input = &self.all_languages();
+    pub fn is_text_seventeen(&self, s: &String) -> Result<bool, String> {
+        let input = &self.in_all_languages();
         for (_, value) in input {
             if value == s {
                 return Ok(true);
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,32 @@
-#![allow(dead_code)]
-
 use seventeen::Seventeen;
 
 fn main() {
     let seventeen = Seventeen;
 
-    println!("Number: {}", seventeen.number());
-    println!("String: {}", seventeen.string());
-    println!("All Languages: {:?}", seventeen.all_languages());
-    println!("English: {:?}", seventeen.english());
-    println!("Korean: {:?}", seventeen.korean());
-    println!("Japanese: {:?}", seventeen.japanese());
-    println!("Kanji: {:?}", seventeen.japanese_kanji());
-    println!("Hiragana: {:?}", seventeen.japanese_hiragana());
-    println!("Katakana: {:?}", seventeen.japanese_katakana());
-    println!("Roman Numeral: {:?}", seventeen.roman_numeral());
-    println!("Base  2: {:?}", seventeen.binary());
-    println!("Base  8: {:?}", seventeen.oct());
-    println!("Base 10: {:?}", seventeen.decimal());
-    println!("Base 16: {:?}", seventeen.hex());
+    println!("Number: {}", seventeen.in_number());
+    println!("String: {}", seventeen.in_string());
+    println!("All Languages: {:?}", seventeen.in_all_languages());
+    println!("English: {:?}", seventeen.in_english());
+    println!("Korean: {:?}", seventeen.in_korean());
+    println!("Japanese: {:?}", seventeen.in_japanese());
+    println!("Kanji: {:?}", seventeen.in_japanese_kanji());
+    println!("Hiragana: {:?}", seventeen.in_japanese_hiragana());
+    println!("Katakana: {:?}", seventeen.in_japanese_katakana());
+    println!("Roman Numeral: {:?}", seventeen.in_roman_numeral());
+    println!("Base  2: {:?}", seventeen.in_binary());
+    println!("Base  8: {:?}", seventeen.in_oct());
+    println!("Base 10: {:?}", seventeen.in_decimal());
+    println!("Base 16: {:?}", seventeen.in_hex());
 
     let str = String::from("dix-sept");
-    if let Ok(_) = seventeen.is_seventeen_text(&str) {
+    if let Ok(_) = seventeen.is_text_seventeen(&str) {
         println!("Yes, '{str}' is seventeen.");
     } else {
         println!("Sorry, '{str}' is not seventeen.");
     }
 
     let str = String::from("XVII");
-    if let Ok(_) = seventeen.is_seventeen_text(&str) {
+    if let Ok(_) = seventeen.is_text_seventeen(&str) {
         println!("Yes, '{str}' is seventeen.");
     } else {
         println!("Sorry, '{str}' is not seventeen.");
